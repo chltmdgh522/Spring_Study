d73b0ea82a8add8df2cedc9b6d977e4e00c848ee
chltmdgh522 <chltmdgh517@naver.com>
2023-03-04 23:08:30 +0900

    core DIP 원칙 적용

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -16,19 +16,21 @@ public class OrderServiceTest {
     //OrderService orderService=new OrderServiceImpl();
     MemberService memberService;
     OrderService orderService;
+
     @BeforeEach //test실행하기전에 무조건 실행
-    public void beforeEach(){
-        AppConfig appConfig=new AppConfig();
-        memberService=appConfig.memberService();
-        orderService=appConfig.orderService();
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
     }
+
     @Test
-    void createOrder(){
-        Long memberID=1L;
-        Member member= new Member(memberID, "최승호", Grade.VIP);
+    void createOrder() {
+        Long memberID = 1L;
+        Member member = new Member(memberID, "최승호", Grade.VIP);
         memberService.join(member);
 
-        Order order=orderService.createOrder(memberID,"itemA",10000);
+        Order order = orderService.createOrder(memberID, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(2000);
     }
 }
