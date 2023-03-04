064bcc7f4c5f4cea059ce53ba11e58574e9744bb
chltmdgh522 <chltmdgh517@naver.com>
2023-03-04 23:08:14 +0900

    core DIP 원칙 적용

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -6,23 +6,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-   // MemberService memberService=new MemberServiceImpl();
-   MemberService memberService;
+    // MemberService memberService=new MemberServiceImpl();
+    MemberService memberService;
 
-   @BeforeEach //test실행하기전에 무조건 실행
-   public void beforeEach(){
-       AppConfig appConfig=new AppConfig();
-       memberService=appConfig.memberService();
-   }
+    @BeforeEach //test실행하기전에 무조건 실행
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
-    void join(){
+    void join() {
         //given ~~ 주어졌을때
         Member member = new Member(1L, "홍세현", Grade.VIP);
 
         //when ~~ 했을때
         memberService.join(member);
-        Member findMember=memberService.findMember(1L);
+        Member findMember = memberService.findMember(1L);
 
         //then 이렇게 된다
         Assertions.assertThat(member).isEqualTo(findMember);
