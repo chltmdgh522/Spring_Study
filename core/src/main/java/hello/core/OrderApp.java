0010a073b7d1e1a7ebaebe3195946496b15c34ca
chltmdgh522 <chltmdgh517@naver.com>
2023-03-07 23:25:43 +0900

    core 스프링 도입

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -4,15 +4,21 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        /*1. MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();*/
 
-        //  MemberService memberService = new MemberServiceImpl();
-        //OrderService orderService = new OrderServiceImpl();
+        /*2. AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();*/
+
+        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService=applicationContext.getBean("memberService",MemberService.class);
+        OrderService orderService=applicationContext.getBean("orderService",OrderService.class);
 
         long memberId = 1L;
 
