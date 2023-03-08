d8522e6f4c0c593fd802ec54543c43a8d76220c5
chltmdgh522 <chltmdgh517@naver.com>
2023-03-08 23:18:02 +0900

    core 스프링 테스트

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -13,9 +13,9 @@ public class MemberApp {
         /*2.AppConfig appConfig=new AppConfig();
         MemberService memberService=appConfig.memberService();*/
 
-        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         // 이것이 AppConfig에서 @ 이들 관리 anno~~~는 @ 기반으로 만들어진것
-        MemberService memberService=applicationContext.getBean("memberService",MemberService.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "최승호", Grade.BASIC);
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
