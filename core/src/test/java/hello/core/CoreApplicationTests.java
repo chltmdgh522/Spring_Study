4500590699e87503046bba2ab5b30dcf01995508
chltmdgh522 <chltmdgh517@naver.com>
2023-03-12 22:39:12 +0900

    core

--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CoreApplicationTests {
 
 	public static void main(String[] args) {
