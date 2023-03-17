8603c5170beb16ae875e6f1bd2f52c0f3616735c
chltmdgh522 <chltmdgh517@naver.com>
2023-03-17 21:33:58 +0900

    싱글톤

--- core/src/test/java/hello/core/singleton/SingletonService.java
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -1,18 +1,29 @@
 package hello.core.singleton;
 
+import ch.qos.logback.core.model.INamedModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SingletonService {
 
-    private static final SingletonService instance= new SingletonService();
+    private static final SingletonService instance = new SingletonService();
 
-    public static SingletonService getInstance(){
+    public static SingletonService getInstance() {
         return instance;
     }
 
-    private SingletonService(){
+    private SingletonService() {
 
     }
 
-    public void logic(){
+    public void logic() {
         System.out.println("싱글톤 객체 로직 호출");
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        String str = new String();
+        str.
     }
 }
