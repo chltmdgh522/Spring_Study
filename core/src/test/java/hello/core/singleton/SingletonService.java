2128ca35b12a9a3566d07c89cb959294b04ad5dd
chltmdgh522 <chltmdgh517@naver.com>
2023-03-18 23:33:05 +0900

    싱글톤

--- core/src/test/java/hello/core/singleton/SingletonService.java
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -20,9 +20,4 @@ public class SingletonService {
         System.out.println("싱글톤 객체 로직 호출");
     }
 
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        String str = new String();
-        str.
-    }
 }
