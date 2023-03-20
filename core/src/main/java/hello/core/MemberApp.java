594610433c1345c0a64322859159a8e20f2548df
chltmdgh522 <chltmdgh517@naver.com>
2023-03-20 18:50:11 +0900

    컴포넌트 스캔

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,9 +1,6 @@
 package hello.core;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.member.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,7 +9,6 @@ public class MemberApp {
         //1. MemberService memberService = new MemberServiceImpl();
         /*2.AppConfig appConfig=new AppConfig();
         MemberService memberService=appConfig.memberService();*/
-
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         // 이것이 AppConfig에서 @ 이들 관리 anno~~~는 @ 기반으로 만들어진것
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
@@ -22,5 +18,6 @@ public class MemberApp {
         System.out.println("new member =" + member.getName());
         System.out.println("findMember = " + findMember.getGrade());
         System.out.println(findMember);
+
     }
 }
--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -16,10 +16,9 @@ public class OrderApp {
         MemberService memberService = appConfig.memberService();
         OrderService orderService = appConfig.orderService();*/
 
-        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
-        MemberService memberService=applicationContext.getBean("memberService",MemberService.class);
-        OrderService orderService=applicationContext.getBean("orderService",OrderService.class);
-
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
         long memberId = 1L;
 
         Member member = new Member(memberId, "최승호", Grade.VIP);
