d5ba1e37ab53bed5b2ae9c9b8a36a8f1337553cd
chltmdgh522 <chltmdgh517@naver.com>
2023-03-22 22:19:05 +0900

    컴포넌트 컴포넌트

---
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,21 @@
+package hello.core.scan;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class AutoAppConfigTest {
+    @Test
+    public void basicScan(){
+        ApplicationContext ac= new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService=ac.getBean(MemberService.class);
+       assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
