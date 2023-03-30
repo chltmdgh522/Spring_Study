18de0b55b8bfad9e3936545c2f31fad692275373
chltmdgh522 <chltmdgh517@naver.com>
2023-03-30 13:45:49 +0900

    롬복

---
+++ core/src/test/java/hello/core/HelloLombok.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+       HelloLombok helloLombok=new HelloLombok();
+       helloLombok.setName("신기하다");
+       String name=helloLombok.getName();
+        System.out.println(name);
+    }
+}
