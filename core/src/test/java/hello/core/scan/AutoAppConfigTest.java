21cc85b94c88c0e63add1e3f31b2f0e8b7ca023a
chltmdgh522 <chltmdgh517@naver.com>
2023-03-31 21:51:45 +0900

    자동 의존관계

--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,7 +1,10 @@
 package hello.core.scan;
 
 import hello.core.AutoAppConfig;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -16,7 +19,12 @@ public class AutoAppConfigTest {
         ApplicationContext ac= new AnnotationConfigApplicationContext(AutoAppConfig.class);
         MemberService memberService=ac.getBean(MemberService.class);
         System.out.println("----");
+
        assertThat(memberService).isInstanceOf(MemberService.class);
+
+       OrderService been =ac.getBean(OrderServiceImpl.class);
+
+        System.out.println(been);
 
     }
 }
