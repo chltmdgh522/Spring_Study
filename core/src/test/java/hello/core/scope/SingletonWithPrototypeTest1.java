d4f949d9335cffa7bdea7fbabd7fdb14e1984c09
chltmdgh522 <chltmdgh517@naver.com>
2023-04-07 00:35:39 +0900

    싱글톤&프로토

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,41 +2,79 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class SingletonWithPrototypeTest1 {
 
     @Test
-    void prototypeFind(){
+    void prototypeFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
         PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
         bean1.addCount();
-        Assertions.assertThat(bean1.getCount()).isEqualTo(1);
-        
+        assertThat(bean1.getCount()).isEqualTo(1);
+
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
         prototypeBean2.addCount();
-        Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
     }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean1.logic();
+        assertThat(count2).isEqualTo(2);
+        ac.close();
+
+    }
+
+    //@RequiredArgsConstructor
+    @Scope("singleton")
+    static class ClientBean {
+        private final PrototypeBean prototypeBean; //생성시점에 주입
+
+        @Autowired
+        ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
     @Scope("prototype")
-    static class PrototypeBean{
-        private  int count=0;
-        public void addCount(){
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
             count++;
         }
-        public int getCount(){
+
+        public int getCount() {
             return count;
         }
 
         @PostConstruct
-        public void init(){
-            System.out.println("PrototypeBean.init"+this);
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
         }
 
         @PreDestroy
-        public void destroy(){
+        public void destroy() {
             System.out.println("PrototypeBean.destroy");
         }
     }
