66cac85532c76cca2c0c42acb21aa407aae31615
chltmdgh522 <chltmdgh517@naver.com>
2023-04-12 22:31:53 +0900

    정적 컨텐츠 및 MVC

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,5 +13,11 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","홍세현 바보");
         return "hello"; //hello.html로 넘어감
+    }
+   // ${data} 모델에 키값을 갖고와서 치환해주는거
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name="name",req)String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
