f05ccf7a1eab7700f671cd7b2c3c5ef0e1583f22
chltmdgh522 <chltmdgh517@naver.com>
2023-04-15 23:44:45 +0900

    스프링빈과 의존관계

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -6,13 +6,20 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-@RequiredArgsConstructor
 
+//@Service
 public class MemberService {
     private final MemberRepository memberRepository;
+
+    //@Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     public Long join(Member member) {
         validateDuplicateMember(member);
