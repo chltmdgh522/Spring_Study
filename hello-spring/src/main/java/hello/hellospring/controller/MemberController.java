7517dc35a70951d4f234309472242f4f38646d59
chltmdgh522 <chltmdgh517@naver.com>
2023-04-16 00:24:48 +0900

    회원관리사이트 만들기

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,7 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 
@@ -10,7 +14,23 @@ public class MemberController {
     private final MemberService memberService;
 
 
+    @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new") //조회할 때 쓰임
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new") //데이터를 폼 같은데 넣어서 전달
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        System.out.println(member.getName());
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
