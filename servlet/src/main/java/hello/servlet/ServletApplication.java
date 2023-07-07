b760509092af6634fa6a2f9b2ab157e2173927cf
seung_ho_choi.s <chltmdgh517@naver.com>
2023-07-07 11:26:34 +0900

    Hello 서블릿

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan //서블릿 자동등록
+@ServletComponentScan //서블릿을 자동등록
 @SpringBootApplication
 public class ServletApplication {
     public static void main(String[] args) {
