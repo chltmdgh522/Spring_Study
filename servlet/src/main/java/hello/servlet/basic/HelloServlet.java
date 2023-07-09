8d2c16ae6f9922dd9852a2f7d295b2942b8f1675
seung_ho_choi.s <chltmdgh517@naver.com>
2023-07-09 19:03:00 +0900

    HTTP 요청 데이터 조회

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -12,6 +12,8 @@ public class HelloServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
         System.out.println("HelloServlet.service");
         String username=request.getParameter("username");
         System.out.println("username = " + username);
