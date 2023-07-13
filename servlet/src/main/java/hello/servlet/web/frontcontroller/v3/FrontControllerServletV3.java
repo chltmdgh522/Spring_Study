c5b1899f7e982cd3f9add0547c40be85a80ae258
seung_ho_choi.s <chltmdgh517@naver.com>
2023-07-13 11:42:55 +0900

    Model v3 수정

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -43,10 +43,14 @@ public class FrontControllerServletV3 extends HttpServlet {
 
         String viewName = mv.getViewName(); // 논리이름 new-form.... 등
 
-        MyView view = new MyView("/WEB-INF/views/" + viewName + ".jsp");
+        MyView view = viewResolver(viewName);
 
         view.render(mv.getModel(), request, response);
 
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 
     private Map<String, String> createParamMap(HttpServletRequest request) {
