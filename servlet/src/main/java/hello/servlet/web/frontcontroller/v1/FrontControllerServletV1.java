9dfd6b63acbbd20cbdfef5c9cd3ddc7485f165dc
seung_ho_choi.s <chltmdgh517@naver.com>
2023-07-13 10:05:44 +0900

    v1 복습

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -13,16 +13,17 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name="frontControllerServletV1", urlPatterns ="/front-controller/v1/*")
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
 public class FrontControllerServletV1 extends HttpServlet {
 
-    private Map<String, ControllerV1> controllerMap= new HashMap<>();
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
 
-    public FrontControllerServletV1(){
-        controllerMap.put("/front-controller/v1/members/new-form",new MemberFormControllerV1());
-        controllerMap.put("/front-controller/v1/members/save",new MemberSaveControllerV1());
-        controllerMap.put("/front-controller/v1/members",new MemberListControllerV1());
+    public FrontControllerServletV1() {
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
     }
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("FrontControllerServletV1.service");
@@ -31,9 +32,9 @@ public class FrontControllerServletV1 extends HttpServlet {
         ControllerV1 controller = controllerMap.get(requestURI);
         if(controller==null){
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
         }
         controller.process(request,response);
+
 
     }
 }
