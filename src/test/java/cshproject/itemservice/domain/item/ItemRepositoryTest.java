7164fc0f5e6cd157274b49f729e1d294682d6e29
seung_ho_choi.s <chltmdgh517@naver.com>
2023-07-21 13:39:03 +0900

    Initial commit

---
+++ src/test/java/cshproject/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,11 @@
+package cshproject.itemservice.domain.item;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    @Test
+
+}
