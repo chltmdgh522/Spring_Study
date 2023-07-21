319050b6e6ff62622564df4b926e501bc0b7c807
seung_ho_choi.s <chltmdgh517@naver.com>
2023-07-21 11:21:19 +0900

    HTTP 메시지 컨버터

--- src/main/java/hello/springmvc/basic/reponse/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/reponse/ResponseBodyController.java
@@ -6,9 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -50,6 +48,7 @@ public class ResponseBodyController {
         helloData.setAge(20);
         return helloData;
     }
+
 }
 
 
