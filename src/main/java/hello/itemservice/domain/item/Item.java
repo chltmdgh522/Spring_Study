a1343ec2d8259cafaef26949a53b85d9a15af54c
seung_ho_choi.s <chltmdgh517@naver.com>
2023-09-20 00:21:41 +0900

    Bean Validation 에러코드

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,14 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+
 public class Item {
 
     private Long id;
