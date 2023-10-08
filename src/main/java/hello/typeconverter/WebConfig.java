20730b533f6a809cb649c4fce60e90cb2157b154
seung_ho_choi.s <chltmdgh517@naver.com>
2023-10-08 18:37:35 +0900

    포맷터 적용하기

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +13,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIpPortConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIpPortConverter());
+//        registry.addConverter(new IntegerToStringConverter()); //컨버터가 우선순위 먼저 먹혀서
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
