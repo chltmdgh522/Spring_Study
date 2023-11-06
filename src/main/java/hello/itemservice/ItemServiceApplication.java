d1afa34ef6c1fce295e6162c73561c8f7bee07b0
seung_ho_choi.s <chltmdgh517@naver.com>
2023-11-06 23:38:12 +0900

    DB설정 1. 구현체가 2개면 아무거나 등록한다. 이때 이름이 같으면 오류발생 2. 수동 빈과 자동빈 즉 둘다 아무관계 없음 3. componet를 해줬으면 scan을 해주어야함 디폴트가 모두등록이긴함

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,7 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +10,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
-@Import(MemoryConfig.class)
+@Import(MemoryConfig.class) //스프링 구성 클래스에 다른 구성 클래스를 가져와서 해당 클래스의 빈(Bean) 설정을 현재 구성 클래스에 추가하도록 지시합니다.
+// 즉 해당 빈타입을 여기다가 가져다가 쓸려고 다시말해서 빈 수동등록이라고 보면될듯 빈을 여기다 쓰면 @Configuration
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
--- src/main/java/hello/itemservice/config/MemoryConfig.java
+++ src/main/java/hello/itemservice/config/MemoryConfig.java
@@ -20,4 +20,5 @@ public class MemoryConfig {
         return new MemoryItemRepository();
     }
 
+
 }
