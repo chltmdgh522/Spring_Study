8e6e5cbf0b4ab037c7a937d3bac569f220f3edb2
seung_ho_choi.s <chltmdgh517@naver.com>
2024-01-18 16:22:32 +0900

    트랜잭션 전파 활용1 _ 예제 프로젝트 시작~!

---
+++ Spring_DB2/SpringDB2_Transaction/src/main/java/hello/springtx/propagation/Log.java
@@ -0,0 +1,24 @@
+package hello.springtx.propagation;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String message;
+
+    public Log(){
+
+    }
+
+    public Log(String message) {
+        this.message = message;
+    }
+}
